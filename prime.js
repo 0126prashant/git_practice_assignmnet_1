a1dd32bdea1f99ff4a079281b460581d7edb6507
Sankalp S <sankalp.sas10@gmail.com>
2022-12-01 11:06:14 +0530

    Assignment Completed

--- prime.js
+++ prime.js
@@ -1,6 +1,14 @@
-let n = 13 ; 
+let n=13;
+let count=0;
 
-for(let i=0 ; i<=n ; i++){
-    if (n%i==0){
-   count++
+for(let i=1 ; i<=n ; i++){
+    if(n%i==0){
+     count++;
+  }
+}
+
+if(count<=2){
+console.log("It is a Prime Number");
+}else{
+console.log("It is not a Prime Number");
 }
